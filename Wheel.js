b245fcc6ae4a2a2d4f349b8b5cd559b669571504
251801008@qq.com <love5201314>
2018-05-05 21:00:29 +0800

    npm init 添加源码

---
+++ Wheel.js
@@ -0,0 +1,145 @@
+import React, { PureComponent } from 'react';
+import { AppSizes } from '../../theme';
+import { View, PanResponder, Animated } from 'react-native';
+import propTypes from 'prop-types';
+
+let startTime = 0;
+const deceleration = 0.0006;
+
+export default class WheelPicker extends PureComponent {
+
+    static propTypes = {
+        data: propTypes.array.isRequired,
+        renderItem: propTypes.func.isRequired,
+        itemHeight: propTypes.number.isRequired,
+        // animatedHeight: propTypes.number.isRequired,
+        wheelStyle: propTypes.object,
+        itemContainerStyle: propTypes.object,
+        wheelPanelHeight: propTypes.number,
+        onChangeSelected: propTypes.func,
+        wheelBackgroundColor: propTypes.string,
+    };
+
+    static defaultProps = {
+        renderItem: null,
+        wheelPanelHeight: AppSizes.screen.height * 0.3,
+    };
+
+    constructor(props) {
+        super(props);
+        this.initialTop = (props.wheelPanelHeight - props.itemHeight) / 2;
+        this.currentY = this.initialTop;
+        this.state = {
+            top: new Animated.Value(this.initialTop),
+        };
+        this.sensitivity = this.props.sensitivity || 0.5;
+        this.index = 0;
+    }
+
+    componentWillMount() {
+        this._panResponder = PanResponder.create({
+            // 要求成为响应者：
+            onStartShouldSetPanResponder: () => true,
+            onStartShouldSetPanResponderCapture: () => true,
+            onMoveShouldSetPanResponder: () => true,
+            onMoveShouldSetPanResponderCapture: () => true,
+
+            onPanResponderGrant: () => {
+                this.state.top.stopAnimation();
+                startTime = new Date().getTime();
+                // 开始手势操作。给用户一些视觉反馈，让他们知道发生了什么事情！
+
+                // gestureState.{x,y} 现在会被设置为0
+            },
+            onPanResponderMove: (evt, gestureState) => {
+                this.state.top.setValue(this.currentY + gestureState.dy);
+                // this.setState({
+                //     top: ,
+                // });
+            },
+            onPanResponderRelease: (evt, gestureState) => {
+                const time = new Date().getTime() - startTime;
+                const endTop = 0 - ((this.props.itemHeight * this.props.data.length)
+                    - ((this.props.wheelPanelHeight + this.props.itemHeight) / 2)) || 0;
+                if (time < 300) {
+                    const distance = gestureState.dy * (this.sensitivity || 1);
+                    const speed = Math.abs(distance) / time;
+                    const speed2 = (this.factor || 1) * speed;
+                    const destination = this.currentY + (((speed2 * speed2) / (2 * deceleration)) * (distance < 0 ? -1 : 1));
+                    this.currentY = destination;
+                }
+                this.currentY += gestureState.dy;
+                const index = ~~((this.initialTop - this.currentY) / this.props.itemHeight);
+                const outItem = ((this.initialTop - this.currentY) % this.props.itemHeight) || 0;
+                if (this.currentY > this.initialTop) {  // 下拉回弹
+                    this.currentY = this.initialTop;
+                    this.index = 0;
+                } else if (this.currentY < endTop) {  // 上拉回弹
+                    this.currentY = endTop;
+                    this.index = this.props.data.length - 1;
+                } else if (outItem > this.props.itemHeight / 2) {
+                    // 如果超出部分 > itemHeight 一半
+                    this.currentY -= (this.props.itemHeight - outItem);
+                    this.index = index + 1;
+                } else {
+                    this.currentY += outItem;
+                    this.index = index;
+                }
+                if (this.props.onChangeSelected) this.props.onChangeSelected(this.props.data[this.index]);
+                Animated.spring(this.state.top, { toValue: this.currentY }).start();
+                // if (this.currentY < this.props.animatedHeight)
+                // this.setState({
+                //     top: this.currentY + gestureState.dy,
+                // });
+                // 用户放开了所有的触摸点，且此时视图已经成为了响应者。
+                // 一般来说这意味着一个手势操作已经成功完成。
+            },
+            onPanResponderTerminationRequest: () => true,
+            onPanResponderTerminate: () => {
+                // 另一个组件已经成为了新的响应者，所以当前手势将被取消。
+                // Animated.spring(this.state.top, { toValue: { x: 0, y: 0 } }).start();
+            },
+            onShouldBlockNativeResponder: () => true,
+        });
+    }
+
+    getDateIndex = () => this.index;
+
+    render = () => (
+        <View
+            style={[
+                {
+                    flex: 1,
+                    backgroundColor: this.props.wheelBackgroundColor || '#FFF',
+                },
+                this.props.wheelStyle
+            ]}
+            {...this._panResponder.panHandlers}
+        >
+            <Animated.View
+                style={{
+                    position: 'absolute',
+                    top: this.state.top,
+                }}
+            >
+                {
+                    this.props.data && this.props.data.map((item, index) => (
+                        <View
+                            style={[
+                                {
+                                    height: this.props.itemHeight,
+                                    width: AppSizes.screen.width,
+                                    justifyContent: 'center',
+                                },
+                                this.props.itemContainerStyle,
+                            ]}
+                            key={index}
+                        >
+                            { this.props.renderItem(item, index) }
+                        </View>
+                    ))
+                }
+            </Animated.View>
+        </View>
+    );
+}
---
+++ WheelPicker.js
@@ -0,0 +1,238 @@
+/**
+ * @author Avery_zjz
+ * @time 2018.4.13
+ * this component is a wheel picker, can be used in android &ios
+ * */
+import React, { PureComponent } from 'react';
+import { AppColors, AppStyles, AppSizes } from '../../theme';
+import { View, Text, StyleSheet, Animated, TouchableOpacity } from 'react-native';
+import { scaleSize } from '@constants/ScreenUtil';
+import Wheel from './Wheel';
+import propTypes from 'prop-types';
+
+export default class WheelPicker extends PureComponent {
+
+    static propTypes = {
+        data: propTypes.array.isRequired, // 渲染数据
+        renderItem: propTypes.func.isRequired, // 渲染Item组件
+        itemHeight: propTypes.number, // 定义滑动item高度
+        wheelPanelHeight: propTypes.number, // 定义滚轮面板高度
+        topBarHeight: propTypes.number,
+        onConfirmTextClick: propTypes.func,
+        onCancelTextClick: propTypes.func,
+        maskContainerStyle: propTypes.object,
+        contentContainerStyle: propTypes.object,
+        topBarStyle: propTypes.object,
+        centerTitleStyle: propTypes.object,
+        cancelComponent: propTypes.func,
+        cancelText: propTypes.string,
+        centerText: propTypes.string,
+        confirmText: propTypes.string,
+        confirmComponent: propTypes.func,
+        centerComponent: propTypes.func,
+        itemContainerStyle: propTypes.object,
+        topLineStyle: propTypes.object,
+        bottomLineStyle: propTypes.object,
+        onChangeSelected: propTypes.func,
+        wheelBackgroundColor: propTypes.string,
+    };
+
+    static defaultProps = {
+        wheelPanelHeight: AppSizes.screen.height * 0.3,
+        topBarHeight: scaleSize(80),
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            showPicker: new Animated.Value(0),
+            data: props.data,
+        };
+    }
+
+    componentWillMount() {
+        this.opacityAnimate = this.state.showPicker.interpolate({
+            inputRange: [0, 0.5, 1],
+            outputRange: [0, 0, 1],
+        });
+        this.bottomAnimate = this.state.showPicker.interpolate({
+            inputRange: [0, 1],
+            outputRange: [-AppSizes.screen.height, 0],
+        });
+        // setTimeout(() => {
+        //     this.showPicker();
+        // }, 2000);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.data !== this.props.data) {
+            this.setState({
+                data: nextProps.data,
+            });
+        }
+    }
+
+    showPicker = () => {
+        Animated.timing(this.state.showPicker, {
+            toValue: 1,
+            duration: 300,
+        }).start();
+    };
+
+    disMissPicker = () => {
+        Animated.timing(this.state.showPicker, {
+            toValue: 0,
+            duration: 300,
+        }).start();
+    };
+
+    textClickCallBack = (callBack) => {
+        this.disMissPicker();
+        const dataItem = this.props.data[this.wheel.getDateIndex()];
+        if (callBack) callBack(dataItem);
+    };
+
+    render = () => {
+        console.log('wheelPanelHeight: ', this.props.wheelPanelHeight);
+        console.log('itemHeight: ', this.props.itemHeight);
+        console.log('final: ', (this.props.wheelPanelHeight - this.props.itemHeight) / 2);
+        return (
+            // 遮罩层
+            <Animated.View
+                style={[
+                    {
+                        height: AppSizes.screen.height,
+                        opacity: this.opacityAnimate,
+                        bottom: this.bottomAnimate,
+                        backgroundColor: 'rgba(0,0,0,0.3)'
+                    },
+                    styles.select_mask,
+                    this.props.maskContainerStyle,
+                ]}
+            >
+                <TouchableOpacity
+                    onPress={() => {
+                        this.disMissPicker();
+                    }}
+                    style={AppStyles.flex1}
+                />
+                {/* content */}
+                <View style={[styles.select_content, this.props.contentContainerStyle]}>
+                    {/* topBar */}
+                    <View
+                        style={[
+                            { height: this.props.topBarHeight },
+                            styles.select_topBar, AppStyles.row,
+                            this.props.topBarStyle
+                        ]}
+                    >
+                        <TouchableOpacity
+                            activeOpacity={this.props.calcelActiveOpacity || 0.9}
+                            style={this.props.cancelContainerStyle}
+                            onPress={() => {
+                                this.textClickCallBack(this.props.onCancelTextClick);
+                            }}
+                        >
+                            {
+                                this.props.cancelComponent || (
+                                    <Text>
+                                        {this.props.cancelText || '取消'}
+                                    </Text>
+                                )
+                            }
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            activeOpacity={this.props.calcelActiveOpacity || 0.9}
+                            style={this.props.cancelContainerStyle}
+                            onPress={() => {
+                                this.textClickCallBack(this.props.onCenterComponentClick);
+                            }}
+                        >
+                            {
+                                this.props.centerComponent || (
+                                    <Text
+                                        style={this.props.centerTitleStyle}
+                                    >
+                                        {this.props.centerText || '请选择'}
+                                    </Text>
+                                )
+                            }
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            activeOpacity={this.props.calcelActiveOpacity || 0.9}
+                            style={this.props.cancelContainerStyle}
+                            onPress={() => {
+                                this.textClickCallBack(this.props.onConfirmTextClick);
+                            }}
+                        >
+                            {
+                                this.props.confirmComponent || (
+                                    <Text>
+                                        {this.props.confirmText || '确认'}
+                                    </Text>
+                                )
+                            }
+                        </TouchableOpacity>
+                    </View>
+                    {/* wheels */}
+                    <View
+                        style={{ height: this.props.wheelPanelHeight }}
+                    >
+                        <Wheel
+                            ref={wheel => { this.wheel = wheel; }}
+                            renderItem={this.props.renderItem}
+                            itemHeight={this.props.itemHeight}
+                            data={this.state.data}
+                            wheelStyle={this.props.wheelStyle}
+                            wheelPanelHeight={this.props.wheelPanelHeight}
+                            itemContainerStyle={this.props.itemContainerStyle}
+                            onChangeSelected={this.props.onChangeSelected}
+                            wheelBackgroundColor={this.props.wheelBackgroundColor}
+                        />
+                        {/* top line */}
+                        <View
+                            style={[
+                                { top: (this.props.wheelPanelHeight - this.props.itemHeight) / 2 },
+                                styles.select_line,
+                                this.props.topLineStyle,
+                            ]}
+                        />
+                        {/* bottom line */}
+                        <View
+                            style={[
+                                { top: (this.props.wheelPanelHeight + this.props.itemHeight) / 2 },
+                                styles.select_line,
+                                this.props.bottomLineStyle,
+                            ]}
+                        />
+                    </View>
+                </View>
+            </Animated.View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    select_mask: {
+        flex: 1,
+        position: 'absolute',
+    },
+    select_content: {
+        width: AppSizes.screen.width,
+    },
+    select_topBar: {
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        backgroundColor: AppColors.white,
+        borderColor: AppColors.placeholder,
+        borderTopWidth: scaleSize(1),
+        borderBottomWidth: scaleSize(1),
+        width: AppSizes.screen.width,
+    },
+    select_line: {
+        backgroundColor: '#d5d5d5',
+        height: scaleSize(1),
+        width: AppSizes.screen.width,
+        position: 'absolute',
+    },
+});
